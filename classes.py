d513bd78fc5cccb6d2101f143e250b796e41db07
nathaliask <kanisnathalia@gmail.com>
2019-12-05 17:06:58 -0300

    oi

--- classes.py
+++ classes.py
@@ -7,8 +7,12 @@ arq = "loja.db"
 db = SqliteDatabase(arq)
 
 class BaseModel(Model):
-    class meta():
+    class Meta:
         database = db
+
+class Genero(BaseModel):
+    nome = CharField()
+    descricao = CharField()
 
 class Instrumento(BaseModel):
     nome = CharField()
@@ -16,69 +20,59 @@ class Instrumento(BaseModel):
     preco = FloatField()
     id_codigo = IntegerField()
 
-class Vendedor(Model):
+class Vendedor(BaseModel):
     nome = CharField()
     cpf = CharField()
 
-class Pedido(Model):
+class Pedido(BaseModel):
     instrumento = ForeignKeyField(Instrumento)
     num_pedido = IntegerField()
     vendedor = ForeignKeyField(Vendedor)
 
-class Genero(Model):
-    nome = CharField()
-    descricao = CharField()
-
-class Fornecedor(Model):
+class Fornecedor(BaseModel):
     nome = CharField()
     cnpj = CharField()
     produtos = CharField()
 
-class Cliente(Model):
+class Cliente(BaseModel):
     nome = CharField()
     cpf = CharField()
     telefone = CharField()
 
-class Venda(Model):
+class Venda(BaseModel):
     pedidos = ManyToManyField(Pedido)
     cliente = ForeignKeyField(Cliente)
     vendedor = ForeignKeyField(Vendedor)
     data = DateField()
     forma_compra = CharField()
 
-class Site(Model):
+class Catalogo(BaseModel):
+    estoque = ManyToManyField(Instrumento)
+
+class Site(BaseModel):
     link = CharField()
     produtos_disp = ForeignKeyField(Catalogo)
     contatos = CharField()
 
-class Local(Model):
+class Local(BaseModel):
     cep = CharField()
     rua = CharField()
     numero = IntegerField()
     bairro = CharField()
     cidade = CharField()
 
-class Catalogo(Model):
-    estoque = ManyToManyField(Insrumento)
-
-if __name__ == "__main__":
-    arq = "loja .db"
-    if os.path.exists(arq):
-        os.remove(arq)
-
-try:
-        db.connect()
-        db.create_tables([Instrumento, Vendedor, Pedido, Genero, Fornecedor, Cliente, Venda, Venda.pedidos.get_through_model(), Site, Local, Catalogo, Catalogo.estoque.get_through_model()])
-
-except OperationError as erro:
-        print("erro ao criar as tabelas: "+str(erro))
-
+#if __name__ == "__main__":
+arq = "loja .db"
+if os.path.exists(arq):
+    os.remove(arq)
+db.connect()
+db.create_tables([Instrumento, Vendedor, Pedido, Genero, Fornecedor, Cliente, Venda, Venda.pedidos.get_through_model(), Site, Local, Catalogo, Catalogo.estoque.get_through_model()])
 
 luana = Vendedor.create(nome="luana", cpf="123.456.789.00")
 vendedorlist= list(map(model_to_dict, Vendedor.select()))
 samba = Genero.create(nome= "Samba", descricao= "Gênero musical e dança com origem na cidade brasileira do Rio de Janeiro.")
 generolist= list(map(model_to_dict, Genero.select()))
-multisom = Fornecedor.creator(nome= "Multisom", cnpj= "04.112.118.0001-62", produtos= "Cavaquinho")
+multisom = Fornecedor.create(nome= "Multisom", cnpj= "04.112.118.0001-62", produtos= "Cavaquinho")
 fornecedorlist= list(map(model_to_dict, Fornecedor.select()))
 endereço = Local.create(cep= "89037-385", rua= "Rua Sabiá", numero= "85", bairro= "Água Verde", cidade= "Blumenau")
 locallist = list(map(model_to_dict, Local.select()))
@@ -88,12 +82,12 @@ pedido1 = Pedido.create(instrumento= cavaquinho, num_pedido= 1, vendedor= luana)
 pedidolist = list(map(model_to_dict, Pedido.select()))
 guilherme = Cliente.create(nome= "Guilherme", cpf= "046.378.439-01", telefone= "3333-3333")
 clientelist = list(map(model_to_dict, Cliente.select()))
-venda1 = Venda.create(pedidos= pedido1, cliente= guilherme, vendedor= luana, data= "2019-12-02", forma_compra= "Pelo site.")
-catalogo1 = Catalogo.create(estoque= cavaquinho)
+venda1 = Venda.create(cliente= guilherme, vendedor= luana, data= "2019-12-02", forma_compra= "Pelo site.")
+catalogo1 = Catalogo.create()
 site1 = Site.create(link= "site.com", produtos_disp= catalogo1, contatos= "7777-7777")
 sitelist = list(map(model_to_dict, Site.select()))
-venda.pedidos.add(pedido1)
-catalogo.estoque.add(cavaquinho)
+venda1.pedidos.add(pedido1)
+catalogo1.estoque.add(cavaquinho)
 vendalist = list(map(model_to_dict, Venda.select()))
 catalogolist = list(map(model_to_dict, Catalogo.select()))
 
